5be194b127b125bb000ce1087c797e302d6cd3a5
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-10-30 22:14:01 -0400

    DOM Nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "BRUNO-ROSSI is the champion";
+
+// const h1 = document.getElementsByTagName("h1");
+
+// h1.setAttribute("id", "victory");
